baa50f96c49dbe782c5ef6a02f4fdb25a270601f
joeyclapton <joeyclapton42@gmail.com>
2023-09-06 15:41:46 -0300

    feat: add others rq

--- lab-medicao-experimentacao-software/sprint-01/app.py
+++ lab-medicao-experimentacao-software/sprint-01/app.py
@@ -1,7 +1,7 @@
 import requests
 import csv
 from utils import getRepositoryAge
-from graph import gen_box_plot
+from chart import box_plot, bar
 from github_api import getToken, getQuery
 
 github_token = getToken()
@@ -47,28 +47,35 @@ with open('dados_repositorios.csv', mode='w', newline='') as file:
     repo_ages = []
     repo_pr_merged = []
     repo_releases = []
+    repo_last_update = []
+    repo_language = []
+    repo_closed_issues = []
 
     for repository_item in data_list:
         repo_info = repository_item['node']
         repo_name = repo_info['name']
         created_at = getRepositoryAge(repo_info['createdAt'])
+        updated_At = getRepositoryAge(repo_info['updatedAt'])
         primary_language = repo_info['primaryLanguage']['name'] if repo_info.get('primaryLanguage') else None
         pull_requests_merged = repo_info['pullRequests']['totalCount']
         releases_count = repo_info['releases']['totalCount']
         closed_issues_count = repo_info['issues']['totalCount']
         total_issues_count = repo_info['totalIssues']['totalCount']
-
-        repo_ages.append(created_at)
-        repo_pr_merged.append(pull_requests_merged)
-        repo_releases.append(total_issues_count)
-
+        
         if total_issues_count > 0:
             ratio_closed_issues = closed_issues_count / total_issues_count
         else:
             ratio_closed_issues = 0.0
 
+        repo_ages.append(created_at)
+        repo_pr_merged.append(pull_requests_merged)
+        repo_releases.append(releases_count)
+        repo_last_update.append(updated_At)
+        repo_language.append(primary_language)
+        repo_closed_issues.append(ratio_closed_issues)
+
         writer.writerow(['Name - ' + repo_name])
-        writer.writerow(['Days on created - ' + str(created_at)])
+        writer.writerow(['Created ago ' + str(created_at) + ' days'])
         writer.writerow(['Primary Language - ' + str(primary_language)])
         writer.writerow(['Releases count - ' + str(releases_count)])
         writer.writerow(['Ratio closed issues - ' + str(ratio_closed_issues)])
@@ -76,9 +83,12 @@ with open('dados_repositorios.csv', mode='w', newline='') as file:
         # Adicione uma linha em branco entre os conjuntos de dados
         writer.writerow([])
         writer.writerow([])
-
-    gen_box_plot(repo_ages, 'Idade dos repositórios', 'Dias')
-    gen_box_plot(repo_pr_merged, 'Total de PRs', 'Total de pull requests aceitas')
-    gen_box_plot(repo_releases, 'Total de releases', '')
+    
+    box_plot(repo_ages, 'Sistemas populares lançam releases com frequência?', 'Idade do repositório em dias')
+    box_plot(repo_pr_merged, 'Sistemas populares recebem muita contribuição externa?', 'Total de pull requests aceitas')
+    box_plot(repo_releases, 'Sistemas populares lançam releases com frequência?', 'Total de releases')
+    box_plot(repo_last_update, 'Sistemas populares são atualizados com frequência?', ' tempo até a última atualização')
+    bar(repo_language, 'Sistemas populares são escritos nas linguagens mais populares', 'Linguagem utilizada')
+    box_plot(repo_closed_issues, 'Sistemas populares possuem um alto percentual de issues fechadas?', 'Razão entre número de issues fechadas pelo total de issues')
 
 print("Dados salvos em dados_repositorios.csv ✅")
