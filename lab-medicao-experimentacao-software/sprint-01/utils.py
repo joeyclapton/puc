b0756e9d463ca8bc8c64f494e1ed00df52eea802
joeyclapton <joeyclapton42@gmail.com>
2023-09-06 15:42:26 -0300

    styles: variables names

--- lab-medicao-experimentacao-software/sprint-01/utils.py
+++ lab-medicao-experimentacao-software/sprint-01/utils.py
@@ -1,9 +1,9 @@
 from datetime import datetime
 
-def getRepositoryAge(created_at):
+def getRepositoryAge(date_info):
     date = datetime
     
-    created_on = datetime.strptime(created_at, "%Y-%m-%dT%H:%M:%SZ")
+    created_on = datetime.strptime(date_info, "%Y-%m-%dT%H:%M:%SZ")
 
     age = date.now() - created_on
 
