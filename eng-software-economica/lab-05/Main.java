277c27b98921755b0904b1dba09a63e1809b6759
joeyclapton <joeyclapton42@gmail.com>
2023-09-07 18:34:41 -0300

    feat: add comments

--- eng-software-economica/lab-05/Main.java
+++ eng-software-economica/lab-05/Main.java
@@ -5,17 +5,26 @@ public class Main {
         var techLead = new TechLead();
         var manager = new Manager();
 
+        /**
+         * Adicionando rotinas para Developes
+        */
         developer.addRoutine("“Programador codifica”");
         developer.addRoutine("“Programador sai para almoçar”");
         developer.addRoutine("“Programador continua condificando”");
         developer.printRoutine();
-
+        
+        /**
+         * Adicionando rotinas para Tech Leaders
+        */
         techLead.addRoutine("“Líder da equipe distribui tarefas para os programadores”");
         techLead.addRoutine("“Líder da equipe sai para almoçar”");
         techLead.addRoutine("“Líder da equipe auxilia os programadores”");
         techLead.addRoutine("“Líder da equipe encerra o dia de trabalho”");
         techLead.printRoutine();
 
+        /**
+         * Adicionando rotinas para Managers
+        */
         manager.addRoutine("“Gerente de projetos reúne com os líderes das equipes”");
         manager.addRoutine("“Gerente de projetos sai para almoçar”");
         manager.addRoutine("“Gerente de projetos analisa novos requisitos”");
